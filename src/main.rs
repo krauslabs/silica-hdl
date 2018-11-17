b906b8b3ff46792338ffea7cc9033af510a0ad66
Nick Kraus <nick@krauslabs.com>
2018-11-17 11:58:32 -0500

    Add input file selection on CLI command via Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+
+    let matches = clap::App::new("silicac")
+        .arg(clap::Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .takes_value(true)
+            .value_name("FILE")
+            .required(true)
+            .help("The input file to compile"))
+        .get_matches();
+
+    let input_filename = matches.value_of("input").unwrap();
+    let mut input_file = File::open(input_filename).expect("Input file not found");
+    let mut input = String::new();
+    input_file.read_to_string(&mut input).expect("Failed to read file");
+
+    println!("{}", input);
+
 }
