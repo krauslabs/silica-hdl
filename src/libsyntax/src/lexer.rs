a29451bc13fbaed3e4f7a467f0ce738161e15ad2
Nick Kraus <nick@krauslabs.com>
2018-11-29 11:30:40 -0500

    Add errors and testing to lexer
    A lexer error type was added to the lexer module, and implementations
    were further fleshed out. Lexer now implements Iterator in a way that
    should be friendly to the lalrpop parser.
    
    Unit testing was added to the lexer module, which already caught a bug
    where numbers were not allowed in an identifier token.

--- src/libsyntax/src/lexer.rs
+++ src/libsyntax/src/lexer.rs
@@ -3,8 +3,7 @@ use std::str::Chars;
 
 use token::{self, Token};
 
-// pub type Location = usize;
-// pub type LexerItem = (Location, Token, Location);
+pub type LexerItem = Result<Token, LexerError>;
 
 pub struct Lexer<'a> {
     input: Peekable<Chars<'a>>,
@@ -30,6 +29,50 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn is_letter(&mut self, ch: char) -> bool {
+        ch.is_alphabetic() || ch == '_'
+    }
+
+    fn peek_is_letter(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => self.is_letter(ch),
+            None => false,
+        }
+    }
+
+    fn is_number(&mut self, ch: char) -> bool {
+        ch.is_numeric()
+    }
+
+    fn peek_is_number(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => self.is_number(ch),
+            None => false,
+        }
+    }
+
+    fn read_identifier(&mut self, first: char) -> String {
+        let mut ident = String::new();
+        ident.push(first);
+
+        while self.peek_is_letter() || self.peek_is_number() {
+            ident.push(self.read_char().unwrap()); // TODO: unwrap()
+        }
+
+        ident
+    }
+
+    fn read_number(&mut self, first: char) -> String {
+        let mut number = String::new();
+        number.push(first);
+
+        while self.peek_is_number() {
+            number.push(self.read_char().unwrap()); // TODO: unwrap()
+        }
+
+        number
+    }
+
     fn skip_whitespace(&mut self) {
         while let Some(&ch) = self.peek_char() {
             if !ch.is_whitespace() {
@@ -44,101 +87,166 @@ impl<'a> Lexer<'a> {
     		self.read_char();
     	}
     }
+}
 
-    fn is_letter(&mut self, ch: char) -> bool {
-	    ch.is_alphabetic() || ch == '_'
-	}
+impl<'a> Iterator for Lexer<'a> {
+    type Item = LexerItem;
 
-	fn peek_is_letter(&mut self) -> bool {
-        match self.peek_char() {
-            Some(&ch) => self.is_letter(ch),
-            None => false,
-        }
-    }
+    fn next(&mut self) -> Option<LexerItem> {
 
-	fn is_number(&mut self, ch: char) -> bool {
-		ch.is_numeric()
-	}
+        self.skip_whitespace();
 
-	fn read_identifier(&mut self, first: char) -> String {
-        let mut ident = String::new();
-        ident.push(first);
-
-        while self.peek_is_letter() {
-            ident.push(self.read_char().unwrap()); // TODO: unwrap()
-        }
-
-        ident
-    }
-
-    fn read_number(&mut self, first: char) -> String {
-        let mut number = String::new();
-        number.push(first);
-
-        while let Some(&ch) = self.peek_char() {
-            if !self.is_number(ch) {
-                break;
+        match self.read_char() {
+            Some(',') => Some(Ok(Token::Comma)),
+            Some(';') => Some(Ok(Token::Semicolon)),
+            Some(':') => Some(Ok(Token::Colon)),
+            Some('(') => Some(Ok(Token::LeftParen)),
+            Some(')') => Some(Ok(Token::RightParen)),
+            Some('{') => Some(Ok(Token::LeftCurlyBrace)),
+            Some('}') => Some(Ok(Token::RightCurlyBrace)),
+            Some('=') => Some(Ok(Token::Assign)),
+            Some('~') => Some(Ok(Token::Negate)),
+            Some('&') => Some(Ok(Token::BitAnd)),
+            Some('|') => Some(Ok(Token::BitOr)),
+            Some('^') => Some(Ok(Token::BitXor)),
+            Some('<') => { 
+                if self.peek_char_eq('<') {
+                    self.read_char();
+                    Some(Ok(Token::ShiftLeft))
+                } else {
+                    Some(Err(LexerError::InvalidCharacter('<')))
+                }
             }
-            number.push(self.read_char().unwrap()); // TODO: unwrap()
+            Some('>') => { 
+                if self.peek_char_eq('>') {
+                    self.read_char();
+                    Some(Ok(Token::ShiftRight))
+                } else {
+                    Some(Err(LexerError::InvalidCharacter('>')))
+                }
+            }
+            Some('/') => {
+                if self.peek_char_eq('/') {
+                    self.skip_line();
+                    self.next()
+                } else {
+                    Some(Err(LexerError::InvalidCharacter('/')))
+                }
+            }
+            Some(ch @ _) => {
+                if self.is_letter(ch) {
+                    let ident = self.read_identifier(ch);
+                    Some(Ok(token::lookup_ident(&ident)))
+                } else if self.is_number(ch) {
+                    let num = self.read_number(ch);
+                    Some(Ok(Token::Litrl(num)))
+                } else {
+                    Some(Err(LexerError::InvalidCharacter(ch)))
+                }
+            }
+            None => None,
         }
+    }
+}
 
-        number
+#[derive(Debug, PartialEq)]
+pub enum LexerError {
+    InvalidCharacter(char),
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    fn assert_lex(source: &str, expected: Vec<LexerItem>) {
+        let lexer = Lexer::new(source);
+
+        for (idx, item) in lexer.enumerate() {
+            assert_eq!(item, expected[idx])
+        }
     }
 
-    pub fn next_token(&mut self) -> Option<Token> {
+    #[test]
+    fn identifiers() {
+        assert_lex(
+            "id _id id123 modu mo ",
+            vec![
+                Ok(Token::Ident("id".to_string())),
+                Ok(Token::Ident("_id".to_string())),
+                Ok(Token::Ident("id123".to_string())),
+                Ok(Token::Ident("modu".to_string())),
+                Ok(Token::Ident("mo".to_string())),
+            ]
+        );
+    }
 
-    	self.skip_whitespace();
+    #[test]
+    fn literal() {
+        assert_lex(
+            " 1 0123 ",
+            vec![
+                Ok(Token::Litrl("1".to_string())),
+                Ok(Token::Litrl("0123".to_string())),
+            ]
+        );
+    }
 
-    	match self.read_char() {
-    		Some(',') => Some(Token::Comma),
-    		Some(';') => Some(Token::Semicolon),
-    		Some(':') => Some(Token::Colon),
-    		Some('(') => Some(Token::LeftParen),
-    		Some(')') => Some(Token::RightParen),
-    		Some('{') => Some(Token::LeftCurlyBrace),
-    		Some('}') => Some(Token::RightCurlyBrace),
-    		Some('=') => Some(Token::Assign),
-    		Some('~') => Some(Token::Negate),
-    		Some('&') => Some(Token::BitAnd),
-    		Some('|') => Some(Token::BitOr),
-    		Some('^') => Some(Token::BitXor),
-    		Some('<') => { 
-    			if self.peek_char_eq('<') {
-    				self.read_char();
-    				Some(Token::ShiftLeft)
-    			} else {
-    				panic!();
-    			}
-    		}
-    		Some('>') => { 
-    			if self.peek_char_eq('>') {
-    				self.read_char();
-    				Some(Token::ShiftRight)
-    			} else {
-    				panic!();
-    			}
-    		}
-    		Some('/') => {
-    			if self.peek_char_eq('/') {
-    				self.skip_line();
-    				self.next_token()
-    			} else {
-    				None
-    			}
-    		}
-    		Some(ch @ _) => {
-    			if self.is_letter(ch) {
-    				let ident = self.read_identifier(ch);
-    				Some(token::lookup_ident(&ident))
-    			} else if self.is_number(ch) {
-    				let num = self.read_number(ch);
-    				Some(Token::Litrl(num))
-    			} else {
-    				None
-    			}
-    		}
-    		None => None,
-    	}
+    #[test]
+    fn comments() {
+        assert_lex(
+            " top // this shouldn't be seen \n mod ",
+            vec![
+                Ok(Token::Top),
+                Ok(Token::Mod),
+            ]
+        );
+    }
+
+    #[test]
+    fn keywords() {
+        assert_lex(
+            " mod top in out bit ",
+            vec![
+                Ok(Token::Mod),
+                Ok(Token::Top),
+                Ok(Token::In),
+                Ok(Token::Out),
+                Ok(Token::Bit),
+            ]
+        );
+    }
+
+    #[test]
+    fn operators() {
+        assert_lex(
+            " = ~ & | ^ << >> ",
+            vec![
+                Ok(Token::Assign),
+                Ok(Token::Negate),
+                Ok(Token::BitAnd),
+                Ok(Token::BitOr),
+                Ok(Token::BitXor),
+                Ok(Token::ShiftLeft),
+                Ok(Token::ShiftRight),
+            ]
+        );
+    }
+
+    #[test]
+    fn punctuation() {
+        assert_lex(
+            " , ; : ( ) { } ",
+            vec![
+                Ok(Token::Comma),
+                Ok(Token::Semicolon),
+                Ok(Token::Colon),
+                Ok(Token::LeftParen),
+                Ok(Token::RightParen),
+                Ok(Token::LeftCurlyBrace),
+                Ok(Token::RightCurlyBrace),
+            ]
+        );
     }
 }
 
