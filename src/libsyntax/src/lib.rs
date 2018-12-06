3eb976f6be0bbba7f257a595891ca810d77c4930
Nick Kraus <nick@krauslabs.com>
2018-12-06 12:30:19 -0500

    Add additional tests to libsyntax

--- src/libsyntax/src/lib.rs
+++ src/libsyntax/src/lib.rs
@@ -50,6 +50,26 @@ mod test {
 		assert_eq!(expr, expected);
 	}
 
+	fn assert_stmt(source: &str, expected: &Stmt) {
+		let module = format!("top mod a ( out y: bit ) {{ {}; }}", source);
+
+		let ast = Ast::new(&module);
+		let stmt = &ast.top.stmts[0];
+
+		assert_eq!(stmt, expected);
+	}
+
+	#[test]
+	fn assign_stmt() {
+		assert_stmt(
+			"y = 5",
+			&Stmt::Assign{
+				id: "y".to_string(),
+				ex: Expr::Litrl{val: "5".to_string()},
+			}
+		);
+	}
+
  	#[test]
  	fn precedence() {
  		assert_expr(
@@ -77,6 +97,28 @@ mod test {
  		);
  	}
 
+ 	#[test]
+ 	fn paren_grouping() {
+ 		assert_expr(
+ 			"1 << ( 2 | 3 ) >> 1",
+ 			&Expr::Binary{
+ 				lex: Box::new(Expr::Binary{
+ 					lex: Box::new(Expr::Litrl{val: "1".to_string()}),
+ 					op: BinaryOp::ShiftLeft,
+ 					rex: Box::new(Expr::Paren{
+ 						ex: Box::new(Expr::Binary{
+ 							lex: Box::new(Expr::Litrl{val: "2".to_string()}),
+ 							op: BinaryOp::BitOr,
+ 							rex: Box::new(Expr::Litrl{val: "3".to_string()}),
+ 						})
+ 					}),
+ 				}),
+ 				op: BinaryOp::ShiftRight,
+ 				rex: Box::new(Expr::Litrl{val: "1".to_string()}),
+ 			},
+ 		);
+ 	}
+
 	#[test]
 	fn trailing_comma() {
 
@@ -100,5 +142,34 @@ mod test {
 	    let ast_comma = Ast::new(comma);
 
 	    assert_eq!(ast_comma, ast_no_comma);
+	}
+
+	#[test]
+	fn module() {
+		let source = 
+			"top mod a (
+	            out y: bit
+	        ) {
+	            y = 1;
+	        }";
+
+	    let ast = Ast::new(source);
+
+	    let expected = Ast{
+	    	top: Mod{
+	    		name: "a".to_string(),
+	    		ports: vec![Port{
+	    			dir: Dir::Output,
+	    			name: "y".to_string(),
+	    			ty: Type::Bit,
+	    		},],
+	    		stmts: vec![Stmt::Assign{
+	    			id: "y".to_string(),
+	    			ex: Expr::Litrl{val: "1".to_string()},
+	    		},],
+	    	}
+	    };
+
+	    assert_eq!(ast, expected);
 	}
 }
